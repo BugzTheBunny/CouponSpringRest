547a8031fd93c9eb6742cce9a22eea9869dda328
slava <urigami2010@gmail.com>
2019-09-04 08:33:07 +0300

    Removed @Data

--- src/main/java/com/sprj/coupapi/models/Coupon.java
+++ src/main/java/com/sprj/coupapi/models/Coupon.java
@@ -11,10 +11,8 @@ import com.sprj.coupapi.enums.CouponStatus;
 import com.sprj.coupapi.enums.CouponType;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -35,7 +33,63 @@ public class Coupon {
 	@Enumerated(EnumType.STRING)
 	@Column(columnDefinition = "varchar(32) default 'SALE'")
 	private CouponStatus status = CouponStatus.SALE;
+
 	// Date start_date;
 	// Date end_date;
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public CouponType getType() {
+		return type;
+	}
+
+	public void setType(CouponType type) {
+		this.type = type;
+	}
+
+	public CouponStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(CouponStatus status) {
+		this.status = status;
+	}
 
 }
